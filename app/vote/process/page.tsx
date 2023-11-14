47b0838007a330e2ca07aaae3363abd73f47e749
Your Name <bigtulahi1@gmaiil.com>
2023-11-14 03:25:27 +0000

    fix deployment

--- app/vote/process/page.tsx
+++ app/vote/process/page.tsx
@@ -10,7 +10,7 @@ export default async function Process() {
         "As you know, out the nominees chosen, we spin a wheel to pick the cube of the month. What’s your opinion on this method?"
       }
     >
-      <ProcessForm />
+      <ProcessForm placeholder={""} />
     </FormWrapper>
   );
 }
